5fb2e4cb0e2196cb7e3988adaeae53a6858f0e11
maxim <103951335+Makmar1983@users.noreply.github.com>
2022-12-03 12:29:21 +0200

    finish

--- src/components/Profile/ddd.js
+++ src/components/Profile/ddd.js
@@ -20,13 +20,12 @@ export const App = () => {
         stats={user.stats}
       />
       
-    <Statistics title="Upload stats" stats={data} />
+      <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
-      <FriendList friends={friends} />;
-      <TransactionHistory transactions={transactions} />;
-</Container>
-   
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory transactions={transactions} />
+    </Container>
   );
 };
-
-
