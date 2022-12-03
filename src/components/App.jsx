45f7173ee759c1316a00ee2ecc1be21b5bc2651a
maxim <103951335+Makmar1983@users.noreply.github.com>
2022-12-03 12:12:45 +0200

    add5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import user from 'data/user.json';
 import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 
 
 import { Container } from './App.styled';
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -18,7 +22,10 @@ export const App = () => {
         stats={user.stats}
         />
         <Statistics title="Upload stats" stats={data} />
-<Statistics stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
+
         </Container>
    
   );
